0f985e5753e944741f1bc8d66ecbfd69c683137f
Emanuel Fontanha <emanuellira@id.uff.br>
2025-04-04 21:15:17 -0300

    Upando arquivos do projeto

---
+++ script.js
@@ -0,0 +1,34 @@
+function createCommentaryCard() {
+
+    let feedback_div = document.createElement("div")
+    feedback_div.classList.add("feedback_div")
+
+    let feedback_p = document.createElement("p")
+    let nome_p = document.createElement("p")
+    
+    feedback_p.innerText = document.querySelector("#feedback").value
+    nome_p.innerText = document.querySelector("#nome").value
+    
+    feedback_p.classList.add("feedback_p")
+    nome_p.classList.add("feedback_nome")
+
+    feedback_div.append(nome_p)
+    feedback_div.append(feedback_p)
+
+    document.querySelector("#opinions").prepend(feedback_div)
+    document.querySelector("#feedback").value = ""
+    document.querySelector("#nome").value = ""
+}
+
+let button = document.querySelector("#btn_fb")
+button.addEventListener("click", ()=> {
+    
+    let text = document.querySelector("#feedback").value
+    let nome = document.querySelector("#nome").value
+
+    if (!(nome === "" || text === "")) {
+        createCommentaryCard()
+    } else {
+        alert("Preencha todos os campos para deixar um feedback!")
+    }
+})
